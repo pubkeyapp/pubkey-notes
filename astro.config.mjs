0a95015510fba3b3e4d7cedcbf99a7cd43c866ab
Bram Borggreve <beeman@beeman.nl>
2024-01-06 09:38:38 +0000

    feat: add more solana docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,24 +8,51 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'PubKey Notes',
+      expressiveCode: {
+        themes: ['github-dark', 'github-light'],
+      },
       social: {
         github: 'https://github.com/pubkeyapp/pubkey-notes',
       },
       sidebar: [
         {
           label: 'Helius',
-
+          collapsed: true,
           items: [
             { label: 'Overview', link: '/helius' },
             { label: 'Getting Started', link: '/helius/getting-started/' },
-            { label: 'Examples', autogenerate: { directory: 'helius/examples' } },
+            {
+              label: 'Examples',
+              collapsed: false,
+              autogenerate: { directory: 'helius/examples' },
+            },
           ],
         },
         {
-          label: 'Solana',
-          autogenerate: {
-            directory: 'solana',
-          },
+          label: 'Solana CLI',
+          collapsed: true,
+          items: [
+            { label: 'Overview', link: '/solana-cli' },
+            { label: 'Getting Started', link: '/solana-cli/getting-started/' },
+            {
+              label: 'Examples',
+              collapsed: false,
+              autogenerate: { directory: '/solana-cli/examples' },
+            },
+          ],
+        },
+        {
+          label: 'Solana TypeScript SDK',
+          collapsed: true,
+          items: [
+            { label: 'Overview', link: '/solana-ts' },
+            { label: 'Getting Started', link: '/solana-ts/getting-started/' },
+            {
+              label: 'Examples',
+              collapsed: false,
+              autogenerate: { directory: '/solana-ts/examples' },
+            },
+          ],
         },
       ],
       customCss: ['./src/tailwind.css'],
